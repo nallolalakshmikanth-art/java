4479c2937fa6e0a7ae1b2223e9c83d74c50a9ad6
nallolalakshmikanth-art <nallolalakshmikanth@gmail.com>
2026-07-18 14:45:58 +0530

    Add files via upload

---
+++ code space/def.java
@@ -0,0 +1,11 @@
+public class def {
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        int result = add(10, 20);
+        System.out.println(result);
+    }
+}
